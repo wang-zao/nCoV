d757ac25e2a44a6f220f2acbcacb8b7e793e7633
Stupidism <5801015+Stupidism@users.noreply.github.com>
2020-01-27 23:18:41 +0800

    chore(ncov): init with nx

---
+++ apps/real-time-map/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'real-time-map',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/real-time-map'
+};
